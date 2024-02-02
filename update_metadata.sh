b4949bcc09e1351c95e3867ef06ddab83d2e862c
Ace_Radom <sichenradomlyu@gmail.com>
2024-02-02 12:59:10 +0100

    write tlf fonts -> metadata

--- update_metadata.sh
+++ update_metadata.sh
@@ -6,10 +6,12 @@ cd $script_dir
 
 git log -1 --pretty=format:%h > META
 echo >> META
-echo "#FIGfonts" >> META
 # write git commit hash to META
 
 FONTDIRS=`find . -maxdepth 1 -type d | sort`
+
+echo "#FIGfonts" >> META
+echo "processing FIGfonts"
 
 for dir in $FONTDIRS; do
     if [ "$dir" = "./.git" ] || [ "$dir" = "./.github" ] || [ "$dir" = "." ]; then
@@ -31,6 +33,7 @@ for dir in $FONTDIRS; do
 done
 
 echo "#control_files" >> META
+echo "processing control files"
 
 for dir in $FONTDIRS; do
     if [ "$dir" = "./.git" ] || [ "$dir" = "./.github" ] || [ "$dir" = "." ]; then
@@ -41,6 +44,28 @@ for dir in $FONTDIRS; do
     echo " * enter directory: $dir"
     cd $dir
     FONTS=`find . -maxdepth 1 -type f -name "*.flc" | sed "s!.*/\([^/]*\)\.flc!\1!" | sort`
+    count=0
+    for f in $FONTS; do
+        echo "$f:`echo $dir | sed 's/^\.\///'`/$f" >> ../META
+        let count++
+    done
+    echo "find $count control files in this directory"
+    echo " * leave directory: $dir"
+    cd ..
+done
+
+echo "#tlf" >> META
+echo "processing tlf fonts"
+
+for dir in $FONTDIRS; do
+    if [ "$dir" = "./.git" ] || [ "$dir" = "./.github" ] || [ "$dir" = "." ]; then
+        echo " * skip directory: $dir"
+        continue
+    fi
+
+    echo " * enter directory: $dir"
+    cd $dir
+    FONTS=`find . -maxdepth 1 -type f -name "*.tlf" | sed "s!.*/\([^/]*\)\.tlf!\1!" | sort`
     count=0
     for f in $FONTS; do
         echo "$f:`echo $dir | sed 's/^\.\///'`/$f" >> ../META
